ed2978d69d9df479956014f12fd70967a23edc84
Asraful <asrafulasraful166@gmail.com>
2026-06-08 08:52:23 +0600

    announcement bar js added

---
+++ assets/announcement-bar.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const slides = document.querySelectorAll('.nv-announcement-bar-slide');
+  const nextBtn = document.querySelector('.nv-announcement-bar-arrow--next');
+  const prevBtn = document.querySelector('.nv-announcement-bar-arrow--prev');
+});
