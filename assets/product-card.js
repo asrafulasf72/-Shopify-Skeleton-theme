57f14282452063003073b95bbe9d5452934a1838
Asraful <asrafulasraful166@gmail.com>
2026-06-25 10:20:40 +0600

    js updated

--- assets/product-card.js
+++ assets/product-card.js
@@ -14,6 +14,30 @@ function runWhenSwiperReady(callback, attempts) {
   window.setTimeout(function () { runWhenSwiperReady(callback, attempts + 1); }, 100);
 }
 
+class ProductCard extends HTMLElement {
+  constructor() {
+    super();
+    this.swiper              = null;
+    this.defaultFlyToCartImage = '';
+  }
+
+  connectedCallback() {
+    this.cacheElements();
+
+    if (!(this.swiperEl instanceof HTMLElement)) return;
+
+    const slideCount = this.swiperEl.querySelectorAll('.swiper-slide').length;
+
+    if (slideCount < 2) {
+      if (this.sliderControls) this.sliderControls.hidden = true;
+      return;
+    }
+
+    if (this.sliderControls) this.sliderControls.hidden = false;
+    this.defaultFlyToCartImage = this.addToCartForm?.dataset.flyToCartImage || '';
+
+    runWhenSwiperReady(() => this.initSwiper());
+}
 
 if (!customElements.get('product-card')) {
   customElements.define('product-card', ProductCard);
